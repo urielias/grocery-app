fbd9299b2ae853905fbfc50eab5683dd47b046e2
EverBananas <jmd22@student.london.ac.uk>
2023-02-19 15:46:41 -0400

    Reworked layout using Grid

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -6,22 +6,20 @@ import testImg from '../assets/pork_noodles.png'
 
 const RecipeCard = (props: any) => { 
     return (
-        <>
-        <div className = "RecipeCardTitle">
-            <h2>{props.props.name}</h2>
-            <p>Category: {props.props.category}, Difficulty: {props.props.difficulty}</p>
-        </div>
-
-        <div className="Container">
-            <div className="ImageContainer">
+        <div className = "recipe-card-container">
+            <div className = "cell left-cell">
+                <div className = "holder">
+                    <h2 className = "headertext">{props.props.name}</h2>
+                    <p>Category: {props.props.category}, Difficulty: {props.props.difficulty}</p>
                     <img src={testImg} alt={props.props.name} className="Image" />
+                </div>
+            </div>
+            
+            <div className = "cell right-cell">
+            <RecipeCardTabs props = {props.props} />
             </div>
 
-            <div className="TabContainer">
-                <RecipeCardTabs props = {props.props} />
-            </div>
         </div>
-        </>
     )
 }
 
