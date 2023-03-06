c044b9a820379177a2dc122524698e65d68d685e
rfraze <35712509+rfraze@users.noreply.github.com>
2023-03-05 21:00:34 -0600

    Adding static markup and styling for components related to the selected recipes page

---
+++ frontend/src/components/SelectedRecipesContentContainer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SelectedrecipesContentContainer.css";
+import SelectedRecipeCategory from "../components/SelectedRecipeCategory";
+import CreateListBtn from "../components/CreateListBtn";
+
+const SelectedRecipesContentContainer = (props: any) => {
+    return <div className="SelectedRecipesContentContainer">
+                <h1 className="RecipeCategoryTitle">Selected Recipes</h1>
+                <SelectedRecipeCategory/>
+                <CreateListBtn/>
+            </div>
+}
+
+export default ContentContainer;
---
+++ frontend/src/components/SelectedRecipesCreateListBtn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./SelectedRecipesCreateListBtn.css";
+
+
+const SelectedRecipesCreateListBtn = (props: any) => {
+    return (
+        <button class="SelectedRecipesCreateListBtn">Create Shopping list</button>
+    )
+}
+
+export default SelectedRecipesCreateListBtn;
