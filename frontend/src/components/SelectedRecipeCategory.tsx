b9c3e6cbe9f46bd078893542bf732149930c120c
Uri Elias <urieliasaks@hotmail.com>
2023-03-12 01:11:22 -0600

    remove unused files

--- frontend/src/components/SelectedRecipeCategory.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./SelectedRecipeCategory.css";
-
-const SelectedRecipeCategory = (props: any) => {
-    return <div className="SelectedRecipeCategory">{/* recipe card components go here */}</div>;
-};
-
-export default SelectedRecipeCategory;
