374c2887cc498748c5fef65f5cc53ccaebc626fd
Uri Elias <urieliasaks@hotmail.com>
2023-03-12 01:14:59 -0600

    removed dummy components

--- frontend/src/components/RecipeDetailContentContainer.tsx
+++ frontend/src/components/RecipeDetailContentContainer.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "./RecipeDetailContentContainer.css";
-import RecipeDetailTitle from "../components/RecipeDetailTitle";
-import RecipeDetailTags from "../components/RecipeDetailTags";
-import RecipeDetailMain from "../components/RecipeDetailMain";
-
 
 const RecipeDetailContentContainer = (props: any) => {
-    return <div className="RecipeDetailContentContainer">
-              <RecipeDetailTitle/>
-              <RecipeDetailTags/>
-              <RecipeDetailMain/>
+    return (
+        <div className="RecipeDetailContentContainer">
+            <div className="RecipeDetailTitle">
+                <h1>Recipe Title</h1>
+                <img className="DetailAdd" src={""} alt="add recipe to shopping list" />
             </div>
-}
+            <div className="DetailTags">
+                {/* will need to make these tags dynamic */}
+                Lunch, Easy, Healthy
+            </div>
+            <div className="RecipeDetailMain">
+                <div className="RecipeDetailImgContainer">
+                    {/* need to make img dynamic  */}
+                    <img className="RecipeDetailImg" src={""} alt="Image of completed recipe" />
+                </div>
+                <div className="RecipeDetailTableContainer">
+                    <h3 className="TableTitle">Ingredients</h3>
+                    {/* will need to make elements within table dynamic */}
+                    <table className="RecipeDetailIngredients">
+                        <tr className="TableRow">
+                            <td>ingredient 1</td>
+                        </tr>
+                        <tr className="TableRow">
+                            <td>ingredient 2</td>
+                        </tr>
+                        <tr className="TableRow">
+                            <td>ingredient 3</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default RecipeDetailContentContainer;
